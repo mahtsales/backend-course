c9c1be10568a212dcb9653123f01e8319784f130
mahtsales <davimatheusfsales@gmail.com>
2026-04-02 23:53:23 -0400

    feat: add comments about req http get

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const express = require('express');
-const app = express();
+const express = require('express'); //importa o express para aplicacao, atribuindo ele na const 'express'
+const app = express(); //para construir uma aplicacao, criamos uma instancia do app express que atribuimos a const app
 
-app.get('/', function (req,res){
-    res.send('Hello World');
+/*parametros req, res => funçao callback que é chamada quando a rota '/' e acessada.
+req => contem as informaçoes sobre a req HTTP.
+res => usado para responder o cliente, sendo utilizado pelo metodo res.send(), para enviar resposta ao cliente.*/
+app.get('/', function (req,res){ //app.get => usado para definir a rota da req HTTP GET, onde a rota '/' é o caminho principal do site (a rota).
+    res.send('Hello World'); //endpoint prinicipal com a msg 'hello world'
 });
 
 app.listen();
